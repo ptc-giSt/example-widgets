bc6e91ad9b965b0baaad2d6dbf55095dbb1cf14c
Peter Day <pday@ptc.com>
2018-07-26 17:10:22 -0500

    Add brand new test widget for demonstration purposes

---
+++ widgets/testWidget/test.design.js
@@ -0,0 +1,24 @@
+function twxTestWidget() {
+    return {
+        elementTag: 'twx-testWidget',
+        label: 'testWidget',
+        properties: [
+            {
+                name: 'testProp',
+                label: 'Test Property',
+                datatype: 'string',
+                default: 'look at me',
+                isBindingTarget: false
+            }
+        ],
+        designTemplate: function() {
+            return '<span>Test</span>';
+        },
+        runtimeTemplate: function(props) {
+            var template = '<div ng-show="app.fn.isTrue(me.visible)" class="ng-hide testWidget ' + props.class + '"><img class="testWidget" src=""></img></div>';
+            return template;
+        }
+    }
+}
+
+twxAppBuilder.widget('twxTestWidget', twxTestWidget);
